af15db2f7978c82caee6c439bc2f3946b7ec6e8d
Yasanga <yasanga@thesanmark.com>
2024-07-23 09:58:44 +0530

    Add Middleware to Routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+// Public Routes
 Route::get('/', function () {
     return view('welcome');
 });
@@ -11,11 +12,17 @@ Route::get('login', [AuthController::class, 'index'])->name('login');
 Route::post('post-login', [AuthController::class, 'login'])->name('login.post');
 Route::get('registration', [AuthController::class, 'registration'])->name('register');
 Route::post('post-registration', [AuthController::class, 'register'])->name('register.post');
-Route::get('dashboard', [AuthController::class, 'dashboard']);
-Route::get('logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('posts', [PostController::class, 'posts'])->name('posts');
-Route::delete('/posts/delete/{id}', [PostController::class, 'deletePost'])->name('post.delete');
-Route::get('/posts/edit/{id}', [PostController::class, 'editPost'])->name('post.edit');
-Route::patch('/posts/update/{id}', [PostController::class, 'updatePost'])->name('post.update');
-Route::get('/posts/create', [PostController::class, 'createPost'])->name('post.create');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+
+// Authenticated Routes
+Route::middleware('auth')->group(function () {
+    Route::get('dashboard', [AuthController::class, 'dashboard']);
+    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+    // Posts Routes
+    Route::get('posts', [PostController::class, 'posts'])->name('posts');
+    Route::get('/posts/create', [PostController::class, 'createPost'])->name('post.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/edit/{id}', [PostController::class, 'editPost'])->name('post.edit');
+    Route::patch('/posts/update/{id}', [PostController::class, 'updatePost'])->name('post.update');
+    Route::delete('/posts/delete/{id}', [PostController::class, 'deletePost'])->name('post.delete');
+});
